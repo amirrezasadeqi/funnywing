4bc86b3e1f3942dc99b1c0f493cae7f77bb4ca3f
amirrezasadeqi <e.a.sadeqi@gmail.com>
2022-06-16 11:18:53 +0430

    Starting the first step of ros workspace for funnywing.

---
+++ wing_ros_ws/src/wing_navigator/scripts/test_target_getter.py
@@ -0,0 +1,12 @@
+# include headers and libraries
+import rospy
+
+# write your function for example for publisher or ...
+
+if __name__ == '__main__':
+	try:
+		# function()
+	except rospy.ROSInterruptException:
+		pass
+
+
