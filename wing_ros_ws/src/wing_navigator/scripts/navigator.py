7d8472a028c0de08c79b013fc15947beada8667f
amirrezasadeqi <e.a.sadeqi@gmail.com>
2022-06-25 22:22:55 +0430

    Creating a Demo ROS network consisting a navigator and a simple goto Service/Client.

--- wing_ros_ws/src/wing_navigator/scripts/navigator.py
+++ wing_ros_ws/src/wing_navigator/scripts/navigator.py
@@ -1,12 +1,50 @@
-# include headers and libraries
-import rospy
+#!/usr/bin/env python
 
-# write your function for example for publisher or ...
+
+import rospy
+import argparse
+from wing_navigator.srv import *
+from dronekit import connect, Vehicle, VehicleMode, LocationGlobalRelative
+
+def simple_goto_handler(req):
+  try:
+    print("Going to your desired position")
+    vehicle.mode = VehicleMode("GUIDED")
+    goto_location = LocationGlobalRelative(req.lat, req.lon, req.alt)
+    vehicle.simple_goto(goto_location)
+    resp = SimpleGotoResponse()
+    resp.accepted = True
+    return resp
+  except:
+    resp = SimpleGotoResponse()
+    resp.accepted = False
+    return resp
+
+
+def simple_goto_server():
+  s = rospy.Service("/simple_goto", SimpleGoto, simple_goto_handler)
+  print("Ready for goto service")
+  rospy.spin()
 
 if __name__ == '__main__':
-	try:
-		# function()
-	except rospy.ROSInterruptException:
-		pass
 
+  rospy.init_node("navigator")
+
+  parser = argparse.ArgumentParser(description='this is a test program')
+  parser.add_argument('--connect', help='Address of vehicle connection.')
+  args = parser.parse_args()
+  connection_string = args.connect
+
+
+  print("Connecting to vehicle on: %s" % connection_string)
+  vehicle = connect(connection_string, wait_ready=True)
+
+
+  # running the goto server
+  simple_goto_server()
+
+
+
+  print("Close Vehicle connection object!")
+  vehicle.close()
 
--- wing_ros_ws/src/wing_navigator/scripts/test_target_getter.py
+++ wing_ros_ws/src/wing_navigator/scripts/test_target_getter.py
@@ -1,12 +1,33 @@
+#!/usr/bin/env python
+
 # include headers and libraries
 import rospy
+from wing_navigator.srv import *
+import sys
 
-# write your function for example for publisher or ...
+def simple_goto_client(req):
+  rospy.wait_for_service("/simple_goto")
+  try:
+    simple_goto_service = rospy.ServiceProxy("/simple_goto", SimpleGoto)
+    response = simple_goto_service(req)
+    return response.accepted
+  except rospy.ServiceException as e:
+    print("Service Call Failed: %s"%e)
+
+
+def usage():
+  return "%s [lat lon alt]"%sys.argv[0]
+
 
 if __name__ == '__main__':
-	try:
-		# function()
-	except rospy.ROSInterruptException:
-		pass
+  if len(sys.argv) == 4:
+    req = SimpleGotoRequest()
+    req.lat = float(sys.argv[1])
+    req.lon = float(sys.argv[2])
+    req.alt = float(sys.argv[3])
+  else:
+    print(usage())
+    sys.exit(1)
 
-
+  print("Requesting to for Simple goto service to point (lat:%s, lon:%s, alt:%s)"%(req.lat, req.lon, req.alt))
+  print("Request Result: %s"%simple_goto_client(req))
