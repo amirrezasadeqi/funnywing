fe7d57a22d8a8606d8adcdc4a50bd7913368a2c5
amirrezasadeqi <e.a.sadeqi@gmail.com>
2023-10-04 19:31:42 +0330

    Bug Solved:
    1. In conversions of global_position_int_hdg_to_std_msgs_Float64 and global_position_int_relative_alt_to_std_msgs_Float64, Float64 ROS message has no header, and we were assigning header to it. solved!

--- wing_ros_ws/src/wing_navigator/RfCommunication/MAVLinkToRosMsgConverter/ConverterClasses/global_position_int_hdg_to_std_msgs_Float64.py
+++ wing_ros_ws/src/wing_navigator/RfCommunication/MAVLinkToRosMsgConverter/ConverterClasses/global_position_int_hdg_to_std_msgs_Float64.py
@@ -1,5 +1,4 @@
-import rospy
-from std_msgs.msg import Header, Float64
+from std_msgs.msg import Float64
 
 
 class global_position_int_hdg_to_std_msgs_Float64(object):
@@ -17,13 +16,6 @@ class global_position_int_hdg_to_std_msgs_Float64(object):
         """
         # TODO: Set other ROS message fields like header and so on.
         rosMsg = Float64()
-        rosMsg.header = self._getRosMsgHeader()
         # Vehicle heading in GLOBAL_POSITION_INT is in centi degrees.
         rosMsg.data = self._message.hdg / 100.0
         return rosMsg
-
-    def _getRosMsgHeader(self) -> Header:
-        header = Header()
-        header.stamp = rospy.Time.from_sec(self._message.time_boot_ms / 1000.0)
-        header.frame_id = 'gps'
-        return header
--- wing_ros_ws/src/wing_navigator/RfCommunication/MAVLinkToRosMsgConverter/ConverterClasses/global_position_int_relative_alt_to_std_msgs_Float64.py
+++ wing_ros_ws/src/wing_navigator/RfCommunication/MAVLinkToRosMsgConverter/ConverterClasses/global_position_int_relative_alt_to_std_msgs_Float64.py
@@ -1,5 +1,4 @@
-import rospy
-from std_msgs.msg import Header, Float64
+from std_msgs.msg import Float64
 
 
 class global_position_int_relative_alt_to_std_msgs_Float64(object):
@@ -17,13 +16,6 @@ class global_position_int_relative_alt_to_std_msgs_Float64(object):
         """
         # TODO: Set other ROS message fields like header and so on.
         rosMsg = Float64()
-        rosMsg.header = self._getRosMsgHeader()
         # Vehicle relative_alt in GLOBAL_POSITION_INT is in millimeter.
         rosMsg.data = self._message.relative_alt / 1000.0
         return rosMsg
-
-    def _getRosMsgHeader(self) -> Header:
-        header = Header()
-        header.stamp = rospy.Time.from_sec(self._message.time_boot_ms / 1000.0)
-        header.frame_id = 'gps'
-        return header
