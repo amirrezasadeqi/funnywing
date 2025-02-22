1f20b7d041f04a0572feb772bab2f14cdebfc803
amirrezasadeqi <e.a.sadeqi@gmail.com>
2025-02-22 20:04:52 +0330

    Adding missing control interface methods and features for tamron camera.
    1. Implementing set focus mode method for change focus mode between auto and manual.
    
    2. using set focus mode in the setting of the preset at index, since presets must set focus value to specific values and need the focus to be manual.
    
    3. Implementing get zoom method for tamron which gives the real zoom value in terms of zoom level, e.g. 10X, 2X, ...

--- wing_ros_ws/src/wing_navigator/wing_modules/CameraInterface/CameraControlInterfaceImplementation/TamronCameraController.py
+++ wing_ros_ws/src/wing_navigator/wing_modules/CameraInterface/CameraControlInterfaceImplementation/TamronCameraController.py
@@ -32,6 +32,7 @@ class TamronCameraController(CameraControlInterface):
 
     def set_focus(self, focus):
         """
+        Caution: Use set_focus_mode to set focus mode to manual before calling this method.
         @param focus: focus in hexadecimal number, from 0x1000(Far) to 0xB000(Near).
         """
         p, q, r, s = self._get_hex_digits(focus)
@@ -39,6 +40,12 @@ class TamronCameraController(CameraControlInterface):
         self._camera_connection.write(focus_cmd)
 
     def set_preset_at_idx(self, preset_idx, do_mapping=True):
+        """
+        Caution: Calling this method will change camera focus mode to the manual mode! So set focus mode to auto when
+        you need autofocus mode somewhere else.
+        """
+        # Set focus mode to manual to be able to set presets, since a preset is a combination of zoom and focus.
+        self.set_focus_mode("manual")
         if do_mapping:
             # mapping from 0-100 to the preset index in the preset table.
             step_size = 100 / (len(self._preset_table) - 1)
@@ -72,6 +79,66 @@ class TamronCameraController(CameraControlInterface):
         return
 
     def set_focus_mode(self, focus_mode):
-        # TODO: Implement this method
-        rospy.logwarn("This method is not implemented for Tamron camera controller yet.")
-        return
+        """
+        @param focus_mode: "manual" or "auto"
+        @return: True if the focus mode command was written successfully, False otherwise.
+        """
+        focus_mode_map = {
+            "auto": '2',
+            "manual": '3'
+        }
+        set_focus_mode_cmd = bytes.fromhex(f"81 01 04 38 0{focus_mode_map[focus_mode]} FF")
+        try:
+            self._camera_connection.write(set_focus_mode_cmd)
+            return True
+        except Exception as e:
+            rospy.logwarn(f"Failed to set focus mode to {focus_mode}: {e}")
+            return False
+
+    def _get_int_from_zoom_query(self, zoom_query):
+        val = ''
+        for i, d in enumerate(zoom_query[2:-1].hex()):
+            if i % 2:
+                val += d
+        return int(val.upper(), 16)
+
+    def _read_zoom_query(self):
+        """
+        @return: returns a zoom query value as integer number. The value for tamron camera is 0x0000 to 0x4000.
+        """
+        # A buffer to store the last two bytes read from the camera connection for finding the header of the zoom query.
+        buffer = b''
+        while True:
+            # ! Note !: The reading actions may need timeout, otherwise the guidance loop may be blocked. Expiration of
+            # the timeout does not throw any exceptions or errors and just returns empty b'' and I think this can cause
+            # infinite loop or blocking of the guidance loop. So in the future, if there would be some bugs like block
+            # -ing the guidance loop, here can be the potential cause. So if there would be a bug for this, consider
+            # handling it by for example detecting timeouts and returning None for zoom_query. So do that in the future
+            # if you would need it!
+            buffer += self._camera_connection.read(1)
+            if len(buffer) > 2:
+                buffer = buffer[-2:]
+            # zoom query header is "90 50"
+            if buffer == b'\x90\x50':
+                # zoom query is 7 bytes long, starting with '90 50' and ending with 'FF'.
+                zoom_query = buffer + self._camera_connection.read(5)
+                return self._get_int_from_zoom_query(zoom_query)
+
+    def get_zoom(self):
+        try:
+            get_zoom_cmd = bytes.fromhex(f"81 09 04 47 FF")
+            # Flush input buffer before sending command to get the last command response or actual the latest zoom
+            # query. Note that this reset can cause problems if there are other commands by other threads or processes
+            # and this reset may remove the response of those commands. So in the future, if there are other commands
+            # that we need to read their responses, we need to handle this issue for example by using a queue of
+            # commands which must be handled, and they would be handled by a separated dedicated thread and here we just
+            # need the command to the command query.
+            self._camera_connection.reset_input_buffer()
+            self._camera_connection.write(get_zoom_cmd)
+            zoom_query = self._read_zoom_query()
+            # Convert zoom_query(0x0000-0x4000) to zoom_level number with assumption that Tamron is 1X-10X camera.
+            zoom_level = 1.0 + (zoom_query / 16384) * (10.0 - 1.0)
+            return zoom_level
+        except Exception as e:
+            rospy.logwarn(f"Failed to get current zoom level of Tamron camera: {e}")
+            return None
